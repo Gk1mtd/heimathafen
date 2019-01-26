4ae86388251ab9c39bcae1d46c5c4af3351a68b5
MSI\IfT Lars <egal@test.de>
2019-01-26 17:09:14 +0100

    Audioeffekte

--- Heimathafen/Assets/Scripts/Effects.cs
+++ Heimathafen/Assets/Scripts/Effects.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class Effects : MonoBehaviour
 {
+    private AudioSource playerAudioSource;
+
     public ParticleSystem explosion;
     public ParticleSystem sonar;
+    public AudioClip sonarAudioStart;
+    public List<AudioClip> sonarPitch;
     public ParticleSystem stoerkoerper;
+    public AudioClip stoerkoerperAudio;
     public ParticleSystem funkenKollision;
+    public AudioClip funkenKollisionAudio;
+    public AudioClip torpedoLaunch;
 
     public enum Effekte
     {
         Explosion,
         Sonar,
         Stoerkoerper,
-        Funken
+        Funken,
+        TorpedoStart
+    }
+
+    void Start()
+    {
+        playerAudioSource = GameObject.Find("U-Boot-Prefab").GetComponent<AudioSource>();
     }
     
     public void Effekt(Vector3 position, Effekte effekt)
@@ -24,22 +37,29 @@ public class Effects : MonoBehaviour
         {
             case Effekte.Explosion:
                 objekt = Instantiate(explosion, position, Quaternion.identity);
+                StartCoroutine(DestroyEffect(objekt.gameObject));
                 break;
             case Effekte.Sonar:
                 objekt = Instantiate(sonar, position, Quaternion.identity);
+                StartCoroutine(DestroyEffect(objekt.gameObject));
+                playerAudioSource.PlayOneShot(sonarAudioStart);
                 break;
             case Effekte.Stoerkoerper:
+                playerAudioSource.PlayOneShot(stoerkoerperAudio);
                 objekt = Instantiate(stoerkoerper, position, Quaternion.Euler(-195.0f, 90.0f, -90.0f));
+                StartCoroutine(DestroyEffect(objekt.gameObject));
                 break;
             case Effekte.Funken:
                 objekt = Instantiate(funkenKollision, position, Quaternion.identity);
+                StartCoroutine(DestroyEffect(objekt.gameObject));
+                break;
+            case Effekte.TorpedoStart:
+                playerAudioSource.PlayOneShot(torpedoLaunch);
                 break;
             default:
-                objekt = new ParticleSystem();
+                Debug.Log("Fehler in Effects");
                 break;
         }
-
-        StartCoroutine(DestroyEffect(objekt.gameObject));
     }
 
     IEnumerator DestroyEffect(GameObject objekt)
--- Heimathafen/Assets/Scripts/SubAudio.cs
+++ Heimathafen/Assets/Scripts/SubAudio.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SubAudio : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
