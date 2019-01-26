026abe99261c8494430e3b0adfd1c4a08a7c703d
MSI\IfT Lars <egal@test.de>
2019-01-26 20:29:21 +0100

    Mehr Soundeffekte

--- Heimathafen/Assets/Scripts/SubCollision.cs
+++ Heimathafen/Assets/Scripts/SubCollision.cs
@@ -25,12 +25,13 @@ public class SubCollision : MonoBehaviour
         {
             Destroy(collision.gameObject);
             gameMan.ChangeHealth(-34 * dmgModifier);
+            gameMan.effectScript.Effekt(position, Effects.Effekte.Explosion);
         }
         else if (collision.gameObject.CompareTag("Felsen"))
         {
             gameMan.ChangeHealth((-subCont.forwardSpeed * 20 * dmgModifier));
+            gameMan.effectScript.Effekt(position, Effects.Effekte.Funken);
         }
-        gameMan.effectScript.Effekt(position, Effects.Effekte.Explosion);
     }
 
     //Beim Erreichen von Triggern (Oberfläche, Ziel)
