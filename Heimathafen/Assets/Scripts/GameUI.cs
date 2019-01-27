cef86044e5e0cc4dafbb2f1302a6924a17fb596c
MSI\IfT Lars <egal@test.de>
2019-01-27 01:02:29 +0100

    Katzenbilder

--- Heimathafen/Assets/Scripts/GameUI.cs
+++ Heimathafen/Assets/Scripts/GameUI.cs
@@ -7,23 +7,70 @@ public class GameUI : MonoBehaviour
 {
     private Text subtitles;
     public float subtitleDisplayTime;
+    private Image image1;
+    private Image image2;
+    private Color white;
+    private Color transparent;
+
+    private enum UIobjects
+    {
+        Subtitle,
+        Image1,
+        Image2
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         subtitles = GameObject.Find("Subtitles").GetComponent<Text>();
+        image1 = GameObject.Find("SubImg1").GetComponent<Image>();
+        image2 = GameObject.Find("SubImg2").GetComponent<Image>();
+        image1.gameObject.SetActive(false);
+        image2.gameObject.SetActive(false);
+    }
+
+    public void ChangeImage1(Sprite img)
+    {
+        image1.sprite = img;
+        image1.gameObject.SetActive(true);
+        StartCoroutine(RemoveSubtitles(UIobjects.Image1));
+        Debug.Log("img1");
+    }
+
+    public void ChangeImage2(Sprite img)
+    {
+        image2.sprite = img;
+        image2.gameObject.SetActive(true);
+        StartCoroutine(RemoveSubtitles(UIobjects.Image2));
+        Debug.Log("img2");
     }
 
     public void ChangeSubtitles(string subtext)
     {
         subtitles.text = subtext;
         subtitles.gameObject.SetActive(true);
-        StartCoroutine(RemoveSubtitles());
+        StartCoroutine(RemoveSubtitles(UIobjects.Subtitle));
     }
 
-    IEnumerator RemoveSubtitles()
+    IEnumerator RemoveSubtitles(UIobjects obj)
     {
         yield return new WaitForSeconds(subtitleDisplayTime);
-        subtitles.gameObject.SetActive(false);
+        switch (obj)
+        {
+            case UIobjects.Subtitle:
+                subtitles.gameObject.SetActive(false);
+                break;
+            case UIobjects.Image1:
+                image1.gameObject.SetActive(false);
+                break;
+            case UIobjects.Image2:
+                image2.gameObject.SetActive(false);
+                break;
+            default:
+                Debug.Log("Fehler in UI");
+                break;
+        }
+
+
     }
 }
--- Heimathafen/Assets/Scripts/SubCollision.cs
+++ Heimathafen/Assets/Scripts/SubCollision.cs
@@ -55,6 +55,10 @@ public class SubCollision : MonoBehaviour
         if (other.CompareTag("Text"))
         {
             gameMan.GetComponent<GameUI>().ChangeSubtitles(other.GetComponent<SubtitleTrigger>().subtitleText);
+            if (other.GetComponent<SubtitleTrigger>().image1 != null)
+                gameMan.GetComponent<GameUI>().ChangeImage1(other.GetComponent<SubtitleTrigger>().image1);
+            if (other.GetComponent<SubtitleTrigger>().image2 != null)
+                gameMan.GetComponent<GameUI>().ChangeImage2(other.GetComponent<SubtitleTrigger>().image2);
         }
     }
 }
